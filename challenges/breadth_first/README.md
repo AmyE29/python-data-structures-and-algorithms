d3543145be271f4ac001c7e80369f8e51cdac6b3
Amy Evans <amykarline@yahoo.com>
2019-12-30 21:42:48 -0800

    challenge done

---
+++ challenges/breadth_first/README.md
@@ -0,0 +1,9 @@
+### Challenge - Breadth First Traversal
+**Author**: Amy Evans    **Version**: 1.0.0
+
+Write a breadth first traversal method which takes a Binary Tree as its unique input. Without utilizing any of the built-in methods available to your language, traverse the input tree using a Breadth-first approach, and return a list of the values in the tree in the order they were encountered.
+
+#### Solution
+https://github.com/AmyE29/python-data-structures-and-algorithms/blob/master/challenges/assets/breadth_first.jpg
+
+https://github.com/AmyE29/python-data-structures-and-algorithms/blob/master/challenges/breadth_first/breadth_first.py
---
+++ challenges/breadth_first/test_breadth_first.py
@@ -0,0 +1,43 @@
+import pytest
+from breadth_first import BinaryTree, Queue
+
+
+
+def test_add_one():
+    new_tree = BinaryTree()
+    new_tree.add(2)
+    assert new_tree.root.value == 2
+
+def test_add_four():
+    new_tree = BinaryTree()
+    new_tree.add(2)
+    assert new_tree.root.value == 2
+    new_tree.add(7)
+    assert new_tree.root.left.value == 7
+    new_tree.add(5)
+    assert new_tree.root.right.value == 5
+    new_tree.add(2)
+    assert tree._root.left.left.value == 2
+    assert tree._root.left.right == None
+
+def test_breadth_four():
+    new_tree, expected = BinaryTree(), [2,7,5,2]
+    new_tree.add(2)
+    new_tree.add(7)
+    new_tree.add(5)
+    new_tree.add(2)
+    assert new_tree.breadth_first() == expected
+
+def test_breadth_nine():
+    new_tree = BinaryTree()
+    expected = [2,7,5,2,6,9,5,11,4]
+    new_tree.add(2)
+    new_tree.add(7)
+    new_tree.add(5)
+    new_tree.add(2)
+    new_tree.add(6)
+    new_tree.add(9)
+    new_tree.add(5)
+    new_tree.add(11)
+    new_tree.add(4)
+    assert new_tree.breadth_first() == expected
