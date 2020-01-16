46732db767419c5de0249ae5fd453e4f05f21d43
Amy Evans <amykarline@yahoo.com>
2020-01-16 13:04:18 -0800

    fixed the broken stuff

--- challenges/quick_sort/test_quick_sort.py
+++ challenges/quick_sort/test_quick_sort.py
@@ -1,25 +1,31 @@
-from quick_sort import quick_sort
+from quick_sort import quick_sort, partition
 
 def test_unique_values():
-    lst = [8,4,23,42,16,15]
-    expected = [4,8,15,16,23,42]
-    actual = quick_sort(lst)
-    assert actual == expected
+    arr = [8,4,23,42,16,15]
+    quick_sort(arr, 0, len(arr) -1)
+    assert arr == [4,8,15,16,23,42]
 
 def test_doubles_value():
-    lst = [5,12,7,5,5,7,23,16,2,16]
-    expected = [2,5,5,5,7,7,12,16,16,23]
-    actual = quick_sort(lst)
-    assert actual == expected
+    arr = [5,12,7,5,5,7,23,16,2,16]
+    quick_sort(arr, 0, len(arr) -1)
+    assert arr == [2,5,5,5,7,7,12,16,16,23]
 
 def test_negative_values():
-    lst = [8,4,23,-42,16,-15]
-    expected = [-42,-15,4,8,16,23]
-    actual = quick_sort(lst)
-    assert actual == expected
+    arr = [8,4,23,-42,16,-15]
+    quick_sort(arr, 0, len(arr) -1)
+    assert arr == [-42,-15,4,8,16,23]
 
 def test_reverse_values():
-    lst = [[20,18,12,8,5,-2]]
-    expected = [-2,5,8,12,18,20]
-    actual = quick_sort(lst)
-    assert actual == expected
+    arr= [20,18,12,8,5,-2]
+    quick_sort(arr, 0, len(arr) -1)
+    assert arr == [-2,5,8,12,18,20]
+
+def test_part_partition():
+    arr= [0,5,2,6,8,4]
+    actual = partition (arr, 0, len(arr)-1)
+    assert arr == [0,2,4,6,8,5]
+
+def test_quicksort():
+    arr= [0,5,2,6,8,4]
+    quick_sort(arr, 0, len(arr) -1)
+    assert arr == [0,2,4,5,6,8,]
